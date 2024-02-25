b652be5f5c7c9227836a4b4d22d7b79533255f26
Ba6ai <eliseimartelov292@gmail.com>
2024-02-25 23:25:34 +0500

    a

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,9 +8,11 @@ public class EnemyAI : MonoBehaviour
     public List<Transform> patrolPoints;
     public PlayerController player;
     public float viewAngle;
+    public float damage = 30;
 
     private NavMeshAgent _navMeshAgent;
     private bool _isPlaeyrNoticed;
+    private PlayerHealth _playerHealth;
 
     private void Start()
     {
@@ -20,13 +22,25 @@ public class EnemyAI : MonoBehaviour
     private void InitComponentLinks()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _playerHealth = player.GetComponent<PlayerHealth>();
     }
 
     private void Update()
     {
         NoticePlayerUpdate();
         ChaseUpdate();
+        AttackUpdate();
         PatrolUpdate();
+    }
+    private void AttackUpdate()
+    {
+        if (_isPlaeyrNoticed)
+        {
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                _playerHealth.GetComponent<PlayerHealth>().DealDamage(damage * Time.deltaTime);
+            }
+        }
     }
     private void NoticePlayerUpdate()
     {
@@ -48,7 +62,7 @@ public class EnemyAI : MonoBehaviour
     {
         if (!_isPlaeyrNoticed)
         {
-            if (_navMeshAgent.remainingDistance == 0)
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
             {
                 PickNewPatrolPoint();
             }
